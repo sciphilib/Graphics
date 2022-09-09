f545c366808e0d2565e3e2e2ed0895a8f3e0f0cc
sciphilib <sciphilib@gmail.com>
2022-09-09 19:29:50 +0700

    added viewport drawing a triangle

--- Game.cs
+++ Game.cs
@@ -1,20 +1,28 @@
-﻿using OpenTK;
-using OpenTK.Graphics;
-using OpenTK.Windowing.Common;
-using OpenTK.Windowing.Desktop;
-using OpenTK.Graphics.OpenGL;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
 using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Graphics
 {
     public class Game : GameWindow
     {
+
+        private int VBO;
+        private int VAO;
+
+        private Shader? shader;
+
         public int WindowHeight { get; set; }
         public int WindowWidth { get; set; }
         public string WindowTitle { get; set; }
@@ -27,16 +35,66 @@ namespace Graphics
             this.Title = WindowTitle;
         }
 
+        protected override void OnLoad()
+        {
+            shader = new Shader("D:\\Microsoft Visual Studio\\Repos\\Graphics\\Shaders\\VertexShader.glsl", "D:\\Microsoft Visual Studio\\Repos\\Graphics\\Shaders\\FragmentShader.glsl");
+
+            GL.ClearColor(new Color4(0.5f, 0.5f, 0.5f, 1.0f));
+
+            float[] vertices = new float[]
+            {
+                0.0f, 0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                -0.5f, -0.5f, 0.0f
+            };
+
+            VBO = GL.GenBuffer();
+            VAO = GL.GenVertexArray();
+            GL.BindVertexArray(VAO);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+             
+
+            base.OnLoad();
+        }
+
+
+        protected override void OnUnload()
+        {
+            shader?.Dispose();
+            base.OnUnload();
+        }
+
         protected override void OnUpdateFrame(FrameEventArgs args)
         {
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+                Close();
+
+
             base.OnUpdateFrame(args);
         }
 
         protected override void OnRenderFrame(FrameEventArgs args)
         {
-            GL.ClearColor(new Color4(0.5f, 0.5f, 0.5f, 1.0f));
             GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            shader?.Use();
+            GL.BindVertexArray(VAO);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+
             this.Context.SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, e.Width, e.Height);
+            base.OnResize(e);
         }
     }
 }
---
+++ Shader.cs
@@ -0,0 +1,103 @@
+﻿using OpenTK.Compute.OpenCL;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphics
+{
+    public class Shader : IDisposable
+    {
+        public int Handle { get; }
+        private bool disposedValue = false;
+
+        public Shader(string vertexPath, string fragmentPath)
+        {
+            int VertexShader, FragmentShader;
+
+            string VertexShaderSource;
+
+            using (StreamReader reader = new(vertexPath, Encoding.UTF8))
+            {
+                VertexShaderSource = reader.ReadToEnd();
+            }
+
+            string FragmentShaderSource;
+
+            using (StreamReader reader = new(fragmentPath, Encoding.UTF8))
+            {
+                FragmentShaderSource = reader.ReadToEnd();
+            }
+
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+            GL.CompileShader(VertexShader);
+            GL.CompileShader(FragmentShader);
+
+
+            GL.GetShader(VertexShader, ShaderParameter.CompileStatus, out int successV);
+            if (successV == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(VertexShader);
+                System.Console.WriteLine(infoLog);
+            }
+            GL.GetShader(FragmentShader, ShaderParameter.CompileStatus, out int successF);
+            if (successF == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(FragmentShader);
+                System.Console.WriteLine(infoLog);
+            }
+
+            Handle = GL.CreateProgram();
+
+            GL.AttachShader(Handle, VertexShader);
+            GL.AttachShader(Handle, FragmentShader);
+            GL.LinkProgram(Handle);
+
+            GL.GetProgram(Handle, GetProgramParameterName.LinkStatus, out int success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetProgramInfoLog(Handle);
+                Console.WriteLine(infoLog);
+            }
+
+            GL.DetachShader(Handle, VertexShader);
+            GL.DetachShader(Handle, FragmentShader);
+            GL.DeleteShader(FragmentShader);
+            GL.DeleteShader(VertexShader);
+        }
+
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposedValue) return;
+            if (disposing)
+            {
+                // free managed resources
+            }
+            GL.DeleteProgram(Handle);
+            disposedValue = true;
+        }
+
+        ~Shader()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
