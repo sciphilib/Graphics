1bd98f269d692bc3ccf1c1620fe91434ed18c311
sciphilib <sciphilib@gmail.com>
2022-09-09 15:12:19 +0700

    created Viewport

---
+++ Game.cs
@@ -0,0 +1,42 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Mathematics;
+
+namespace Graphics
+{
+    public class Game : GameWindow
+    {
+        public int WindowHeight { get; set; }
+        public int WindowWidth { get; set; }
+        public string WindowTitle { get; set; }
+        public Game(int height, int width, string title) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            WindowHeight = height;
+            WindowWidth = width;
+            WindowTitle = title;
+            this.CenterWindow(new Vector2i(WindowHeight, WindowWidth));
+            this.Title = WindowTitle;
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(new Color4(0.5f, 0.5f, 0.5f, 1.0f));
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            this.Context.SwapBuffers();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Graphics
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new(1280, 800, "Graphics"))
+            {
+                game.Run();
+            }
+        }
+    }
+}
