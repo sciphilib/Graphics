d4d7b66346dcd6048131f93731bf42b651f22715
sciphilib <sciphilib@gmail.com>
2022-09-12 23:56:14 +0700

    added imgui slider, fixed GL.PolygoneMode

--- Renderer.cs
+++ Renderer.cs
@@ -18,10 +18,15 @@ namespace Graphics
         private Camera? _camera;
         private Shader? _shader;
         private GameWindow _window;
+
         // scene variables
         private int VBO;
         private int VAO;
         private bool IsMeshMode = false;
+
+        // imgui variables
+        private System.Numerics.Vector3 _objectPos = System.Numerics.Vector3.Zero;
+        private System.Numerics.Vector3 _objectRot = System.Numerics.Vector3.Zero;
 
         public Renderer(Window window)
         {
@@ -107,37 +112,52 @@ namespace Graphics
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
 
             _shader?.Use();
-            var Mmodel = Matrix4.Identity;
-            Matrix4 Mview;
-            Matrix4 Mprojection;
+            Matrix4 modelMatrix = Matrix4.Identity;
+            Matrix4 viewMatrix;
+            Matrix4 projectionMatrix;
+            Matrix4 rotMatrix;
+            Matrix4 transMatrix;
+
+            Vector3 rotVec = new(_objectRot.X, _objectRot.Y, _objectRot.Z);
+            rotMatrix = Matrix4.CreateRotationX(rotVec.X) * Matrix4.CreateRotationY(rotVec.Y) * Matrix4.CreateRotationZ(rotVec.Z);
+            transMatrix = Matrix4.CreateTranslation(new Vector3(_objectPos.X, _objectPos.Y, _objectPos.Z));
+            modelMatrix = rotMatrix * transMatrix;
+
+
             if (_camera != null)
             {
-                Mview = _camera.GetViewMatrix();
-                Mprojection = _camera.GetProjectionMatrix();
+                viewMatrix = _camera.GetViewMatrix();
+                projectionMatrix = _camera.GetProjectionMatrix();
             }
             else
             {
-                Mview = Matrix4.Identity;
-                Mprojection = Matrix4.Identity;
+                viewMatrix = Matrix4.Identity;
+                projectionMatrix = Matrix4.Identity;
             }
-            _shader?.SetMat4("model", Mmodel);
-            _shader?.SetMat4("view", Mview);
-            _shader?.SetMat4("projection", Mprojection);
+            _shader?.SetMat4("model", modelMatrix);
+            _shader?.SetMat4("view", viewMatrix);
+            _shader?.SetMat4("projection", projectionMatrix);
 
+            GL.PolygonMode(MaterialFace.FrontAndBack, IsMeshMode ? PolygonMode.Line : PolygonMode.Fill);    
             GL.BindVertexArray(VAO);
             GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
         }
 
         private void OnImGuiDraw()
         {
-            ImGui.ShowDemoWindow();
+            ImGui.SliderFloat3("Cube position", ref _objectPos, -5.0f, 5.0f);
+            ImGui.SliderFloat3("Cube rotation", ref _objectRot, MathHelper.DegreesToRadians(0.0f), MathHelper.DegreesToRadians(360.0f));
             Util.CheckGLError("End of frame");
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
         }
 
         private void OnUpdate()
         {
             if (_window.KeyboardState.IsKeyDown(Keys.Escape))
                 _window.Close();
+
+            if (_window.KeyboardState.IsKeyPressed(Keys.Q))
+                IsMeshMode = !IsMeshMode;
         }
 
         private void OnUnload()
